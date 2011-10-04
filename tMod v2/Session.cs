b5c1bee91a80208df57ceaec376033cb2481a7d3
XspeedPL <xspeedpl@windowslive.com>
2011-10-04 20:50:29 +0300

    New, bugless teleport.

--- tMod v2/Session.cs
+++ tMod v2/Session.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -25,6 +25,12 @@ namespace tMod_v3
         public bool Stupidized { get; set; }
         public bool Butterfingers { get; set; }
         public Group Group { get; set; }
+        public int OldSpawnX { get; set; }
+        public int OldSpawnY { get; set; }
+        public int TeleSpawnX { get; set; }
+        public int TeleSpawnY { get; set; }
+        public int SpawnX { get { return MainMod.Player[Index].SpawnX; } }
+        public int SpawnY { get { return MainMod.Player[Index].SpawnY; } }
 
         public Session(int index, string ip)
         {
@@ -114,18 +120,7 @@ namespace tMod_v3
                 IsLoggedIn = true;
                 DbId = id;
                 MainMod.Log(Username + " has logged in under the IP " + IpAddress);
-                if (MainMod.IsOp(Index))
-                {
-                    Group = MainMod.Groups.Ops;
-                }
-                else if (MainMod.IsMod(Index))
-                {
-                    Group = MainMod.Groups.Mods;
-                }
-                else
-                {
-                    Group = MainMod.Groups.GetCustomGroup(Database.GetUserGroup(Username));
-                }
+                Group = MainMod.Groups.GetGroup(Database.GetUserGroup(Username));
                 SendText(0, 255, 0, "Logged in successfully!");
             }
             else
@@ -177,52 +172,105 @@ namespace tMod_v3
         public void SetGroup(string group)
         {
             Console.WriteLine("[NOTICE] Setting {0} to group {1}", Username, group);
-            Group = MainMod.Groups.GetCustomGroup(group);
+            Group = MainMod.Groups.GetGroup(group);
             Database.SetUserGroup(Username, Group.GroupName);
         }
 
-        public void Teleport(int x, int y)
+        public void Teleport(int tx, int ty)
         {
             if (IsUsingTMod)
             {
                 byte[] writeBuffer = new byte[13];
                 Buffer.BlockCopy(BitConverter.GetBytes(9), 0, writeBuffer, 0, 4);
                 writeBuffer[4] = 0xfe;
-                Buffer.BlockCopy(BitConverter.GetBytes(x), 0, writeBuffer, 5, 4);
-                Buffer.BlockCopy(BitConverter.GetBytes(y), 0, writeBuffer, 9, 4);
+                Buffer.BlockCopy(BitConverter.GetBytes(tx), 0, writeBuffer, 5, 4);
+                Buffer.BlockCopy(BitConverter.GetBytes(ty), 0, writeBuffer, 9, 4);
                 NetMessageMod.FlushBuffer(writeBuffer, Index);
             }
             else
             {
-                int spawnTileX = MainMod.SpawnTileX;
-                int spawnTileY = MainMod.SpawnTileY;
-                int oldPlayerX = MainMod.Player[Index].SpawnX;
-                int oldPlayerY = MainMod.Player[Index].SpawnY;
-                MainMod.SpawnTileX = x;
-                MainMod.SpawnTileY = y;
-                NetMessageMod.SendData(0x7, Index);
-                NetMessageMod.SendTileSquare(Index, x, y, 200);
-                if (MainMod.Player[Index].SpawnX >= 0 && MainMod.Player[Index].SpawnY >= 0)
+                if (tx < 0 || ty < 0 || tx >= MainMod.MaxTilesX || ty >= MainMod.MaxTilesY)
                 {
-                    //Invalidate player spawn point
-                    MainMod.Player[Index].SpawnX = x;
-                    MainMod.Player[Index].SpawnY = y;
-                    MainMod.Tile[x, y - 1].active = false;
-                    NetMessageMod.SendTileSquare(Index, x, y - 1, 200);
-                    NetMessageMod.SendData(0xc, Index, -1, "", Index, 0f, 0f, 0f);
-                    MainMod.Tile[x, y - 1].active = true;
+                    Console.WriteLine("Attempt to teleport player {0} to invalid location: {1}, {2}.", Username ?? IpAddress, tx, ty);
+                    return;
                 }
-                else
+                bool changeSpawn = false;
+                int ox = MainMod.SpawnTileX;
+                int oy = MainMod.SpawnTileY;
+                if (SpawnX >= 0 && SpawnY >= 0)
                 {
-                    NetMessageMod.SendData(0xc, Index, -1, "", Index, 0f, 0f, 0f);
+                    changeSpawn = true;
+                    ox = SpawnX;
+                    oy = SpawnY;
                 }
-                MainMod.SpawnTileX = spawnTileX;
-                MainMod.SpawnTileY = spawnTileY;
-                MainMod.Player[Index].SpawnX = oldPlayerX;
-                MainMod.Player[Index].SpawnY = oldPlayerY;
-                NetMessageMod.SendData(0x7, Index);
+                else if (OldSpawnX >= 0 && OldSpawnY >= 0)
+                {
+                    changeSpawn = true;
+                    ox = OldSpawnX;
+                    oy = OldSpawnY;
+                }
+                int sx = tx / 200;
+                int sy = ty / 150;
+                int fromX = Math.Max(0, sx - 1);
+                int fromY = Math.Max(0, sy - 1);
+                int toX = Math.Min(sx + 1, MainMod.MaxTilesX / 200 - 1);
+                int toY = Math.Min(sy + 1, MainMod.MaxTilesY / 150 - 1);
+                int sections = 0;
+                for (int x = fromX; x <= toX; x++)
+                    for (int y = fromY; y <= toY; y++)
+                        if (!NetplayMod.ServerSock[Index].tileSection[x, y]) sections += 1;
+                if (sections > 0)
+                {
+                    NetMessageMod.SendData(9, Index, -1, "Prepairing to teleport...", sections * 150);
+                    for (int x = fromX; x <= toX; x++)
+                        for (int y = fromY; y <= toY; y++)
+                            if (!NetplayMod.ServerSock[Index].tileSection[x, y])
+                                NetMessageMod.SendSection(Index, x, y);
+                    NetMessageMod.SendData(11, Index, -1, "", fromX, fromY, toX, toY);
+                }
+                for (int proj = 0; proj < MainMod.Projectile.Length; ++proj)
+                    if (MainMod.Projectile[proj].active && MainMod.Projectile[proj].owner == Index && (MainMod.Projectile[proj].type == 13 || MainMod.Projectile[proj].type == 32))
+                    {
+                        MainMod.Projectile[proj].active = false;
+                        MainMod.Projectile[proj].type = 0;
+                        NetMessageMod.SendData(27, -1, -1, "", proj);
+                    }
+                int left = 0;
+                int right = -1;
+                if (changeSpawn && oy > 1)
+                {
+                    left = Math.Max(0, ox - 4);
+                    right = Math.Min(ox + 4, MainMod.MaxTilesX);
+                    while (left < MainMod.MaxTilesX && MainMod.Tile[left, oy - 1].type != 79)
+                        left += 1;
+                    while (right > 0 && MainMod.Tile[right, oy - 1].type != 79)
+                        right -= 1;
+                    for (int x = left; x <= right; x++)
+                    {
+                        MainMod.Tile[x, oy - 1].active = false;
+                        NetMessageMod.SendTileSquare(Index, x, oy - 1, 1);
+                        MainMod.Tile[x, oy - 2].active = false;
+                        NetMessageMod.SendTileSquare(Index, x, oy - 2, 1);
+                    }
+                }
+                int tX = MainMod.SpawnTileX, tY = MainMod.SpawnTileY;
+                MainMod.SpawnTileX = tx; MainMod.SpawnTileY = ty;
+                NetMessageMod.SendData(7, Index);
+                MainMod.SpawnTileX = tX; MainMod.SpawnTileY = tY;
+                TeleSpawnX = tx;
+                TeleSpawnY = ty;
+                NetMessageMod.SendData(12, Index, -1, "", Index);
+                int fx = Math.Max(0, Math.Min(MainMod.MaxTilesX - 8, tx - 4));
+                int fy = Math.Max(0, Math.Min(MainMod.MaxTilesY - 8, ty - 4));
+                NetMessageMod.SendTileSquare(Index, fx, fy, 7);
+                if (changeSpawn && oy > 1)
+                {
+                    NetMessageMod.SendTileSquare(Index, ox, oy, 1);
+                    if (right - left >= 0 && oy >= 2)
+                        NetMessageMod.SendTileSquare(Index, left, oy - 2, right - left + 1);
+                }
+                NetMessageMod.SendData(7, Index);
             }
-
         }
     }
 }
